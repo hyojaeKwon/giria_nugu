52a48966ff9ef2e6182f7a92aeace19daf8579c2
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-11-21 22:43:36 +0900

    revise crawling code

--- crawling.py
+++ crawling.py
@@ -2,21 +2,18 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-urlList = [
-  #none
-
-]
-
+urlList = [  # none ]
+nameList = []
+# emailList = []
+phoneList = []
+officeList = []
 for i in range(len(urlList)):
     html = urlopen(urlList[i])
-    nameList =[]
-    emailList = []
-    phoneList = []
-    officeList = []
+
 
     bsObject = BeautifulSoup(html, "html.parser")
 
-    for card in bsObject.find_all("div", {"class": "card--body"}) :
+    for card in bsObject.find_all("div", {"class": "col"}) :
 
         strong = card.find("strong")
         name = strong.get_text()
@@ -25,7 +22,7 @@ for i in range(len(urlList)):
         name = name.replace("[", " ")
         names = name.split()
         name = names[0]
-        # print(name)
+        print(name)
         nameList.append(name)
 
         email = ""
@@ -37,33 +34,36 @@ for i in range(len(urlList)):
         for li in card.find_all("li") :
             category = li.find("b")
             content = li.find("i")
+            print(category)
             # None return case Error Handling
 
-
             category = category.get_text()
-            content = content.get_text()
+            if category=="TEL":
+                content = content.get_text()
 
-            if content == "":
-                content = "정보 없음"
+                if category == "TEL" :
+                    if content == "":
+                        tel = "데이터가 없습니다."
+                    else:
+                        tel = content
 
-            if category == "E-MAIL" :
-                email = content
-                emailList.append(email)
-            elif category == "TEL" :
-                tel = content
-                phoneList.append(tel)
-            elif category == "OFFICE" :
-                office = content
-                officeList.append(office)
 
-        # print(email + " " + tel + " " + office)
-    print(len(nameList))
-    print(len(emailList))
-    information = pd.DataFrame()
-    information['성함'] = nameList
-    information['이메일'] = emailList
-    information['tel'] = phoneList
-    # information['office'] = officeList
+        if tel == "":
+            tel= "데이터가 없습니다."
 
-    infoString = "info_" + str(i)
-    information.to_csv(infoString,index=False)
+        phoneList.append(tel)
+            # elif category == "OFFICE" :
+            #     if content == "":
+            #         office = "데이터가 없습니다."
+            #     else:
+            #         office = content
+            #     officeList.append(office)
+
+        print( tel + " ,  " + office)
+    # print(len(nameList))
+    # print(len(emailList))
+information = pd.DataFrame()
+information['성함'] = nameList
+information['tel'] = phoneList
+# information['office'] = officeList
+information.to_excel("info.xlsx",index=False)
